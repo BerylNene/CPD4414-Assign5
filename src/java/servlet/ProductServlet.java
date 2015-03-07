e7fb917a3801d700ca7e0adbca3191eadcfddaab
Beryl Bawo Nene <nenebawoberyl@gmail.com>
2015-03-07 15:50:09 -0500

    Added a doPut and doDelete

--- src/java/servlet/ProductServlet.java
+++ src/java/servlet/ProductServlet.java
@@ -12,19 +12,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import javax.servlet.annotation.WebServlet;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -82,17 +82,55 @@ public class ProductServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
+     * @param prod
      */
   //  @Override
-    public void doPost(String prod){
+    @POST
+      @Path ("{productId}")
+    public void doPost(String prod) throws ParseException{
+        JSONObject jObject = (JSONObject) new JSONParser().parse(prod);
+        String name = (String) jObject.get("name");
+        String description = (String) jObject.get("description");
+        int quantity =(int) jObject.get("quantity");
+        doPostPut("INSERT INTO products (name, description, quantity) VALUES (?, ?, ?)", name, description, quantity);
+    }
+    
+    private int doPostPut(String query, String name, String description, int quantity){
+        int numChanges = 0;
+        ArrayList prod = new ArrayList();
+        prod.add(name);
+        prod.add(description);
+        prod.add(quantity);
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= prod.size(); i++) {
+                pstmt.setString(i, prod.get(i - 1).toString());
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numChanges;
+    
+    }
+  
+     @PUT
+    @Path("{productId}")
+ // @Override
+    public void doPut(@PathParam("productId")int id, String prod)throws IOException, SQLException{
         JSONObject jObject = new JSONObject();
         String name = (String) jObject.get("name");
-        int description = (int) jObject.get("description");
+        String description = (String) jObject.get("description");
         int quantity =(int) jObject.get("quantity");
         doPostPut("INSERT INTO products (name, description, quantity) VALUES (?, ?, ?, ?)", name, description, quantity);
+         Connection conn = DatabaseConnection.getConnection();
+        String query = "UPDATE products where SET name =\'" + name +"\', description =\'" + description + "\', quantity =\'" + quantity;
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
-    
-    private int doPostPut(String query, String name, int description, int quantity){
+
+
+    private int doPutUpdate(String query, String name, String description, int quantity) {
         int numChanges = 0;
         ArrayList prod = new ArrayList();
         prod.add(name);
@@ -108,37 +146,33 @@ public class ProductServlet {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         return numChanges;
-    
     }
-         
+    
+    @DELETE
+    @Path("{productId}")
+    //@Override
+        public void doDelete(@PathParam("productId")int id) throws SQLException{
+        Connection conn = DatabaseConnection.getConnection();
+        String query = "DELETE from products where productId =" + id;
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.execute();
             
+    }
 
-    
-
-    /**
-     *
-     * @param query
-     * @param params
-     * @return
-     */
-    private int doUpdate(String query, int id, String name, int description, int quantity) {
+    private int delete(String query, int id) {
         int numChanges = 0;
-        ArrayList prod = new ArrayList();
-        prod.add(id);
-        prod.add(name);
-        prod.add(description);
-        prod.add(quantity);
         try (Connection conn = DatabaseConnection.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= prod.size(); i++) {
-                pstmt.setString(i, prod.get(i - 1).toString());
-            }
+                pstmt.setInt(1, id);
             numChanges = pstmt.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
         return numChanges;
     }
+
+
+
 
     /**
      * Returns a short description of the servlet.
@@ -190,73 +224,5 @@ public class ProductServlet {
         }
         return sb.toString();
     }
-    
-   // @Override
-    protected void doPut(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Set<String> keySet = request.getParameterMap().keySet();
-        if (keySet.contains("id") && keySet.contains("name")
-                && keySet.contains("description") && keySet.contains("quantity")) {
-            // There are some parameters
-            String id = request.getParameter("id");
-            int id_1 = Integer.parseInt(id);
-            String name = request.getParameter("name");
-            String description = request.getParameter("description");
-            String quantity = request.getParameter("quantity");
-            int quantity_1 = Integer.parseInt(quantity);
-
-            doPutUpdate("UPDATE products SET id = ?, name = ?, description = ?, quantity = ?", id_1, name, description, quantity_1);
-        } else {
-            // There are no parameters at all
-            response.setStatus(500);
-        }
-    }
-
-
-    private int doPutUpdate(String query, int id, String name, String description, int quantity) {
-        int numChanges = 0;
-        ArrayList prod = new ArrayList();
-        prod.add(id);
-        prod.add(name);
-        prod.add(description);
-        prod.add(quantity);
-        try (Connection conn = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= prod.size(); i++) {
-                pstmt.setString(i, prod.get(i - 1).toString());
-            }
-            numChanges = pstmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return numChanges;
-    }
-    
-    //@Override
-    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Set<String> keySet = request.getParameterMap().keySet();
-        if (request.getParameterNames().hasMoreElements()) {
-            // There are some parameters
-            int id = Integer.parseInt(request.getParameter("id"));
-           doUpdate("DELETE from products where id = ?", id);
-        } else {
-            // There are no parameters at all
-            response.setStatus(500);
-        }
-    }
-
-    private int doUpdate(String query, int id) {
-        int numChanges = 0;
-        try (Connection conn = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-                pstmt.setInt(1, id);
-            numChanges = pstmt.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return numChanges;
-    }
-
-
 }
+  
