b3528d0d1c1e003337dab0447e72b2cdc176d527
Beryl Bawo Nene <nenebawoberyl@gmail.com>
2015-03-07 12:45:42 -0500

    Added a doPostPut

--- src/java/servlet/ProductServlet.java
+++ src/java/servlet/ProductServlet.java
@@ -21,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -56,36 +57,63 @@ public class ProductServlet {
          return jArray.toJSONString();
     }
 
+    @GET
+    @Produces("application/json")
+    @Path ("{productId}")
+    public String doGet(@PathParam("productId")int id)throws IOException, SQLException {
+        JSONObject jObject = new JSONObject();
+        Connection conn = DatabaseConnection.getConnection();
+        String query = "SELECT * FROM products where productId =" + id;
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        
+         ResultSet resultSet  = preparedStatement.executeQuery();
+         while (resultSet.next()){
+             int num_columns = resultSet.getMetaData().getColumnCount();
+             for (int i = 0; i < num_columns; i++){
+                 String columnName = resultSet.getMetaData().getColumnLabel(i+1);
+                 Object columnValue = resultSet.getObject(i+1);
+                 jObject.put(columnName, columnValue);
+             }
+             
+         }
+         return jObject.toJSONString();
+    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
      */
   //  @Override
-    public String doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Set<String> keySet = request.getParameterMap().keySet();
-        if (keySet.contains("id") && keySet.contains("name")
-                && keySet.contains("description") && keySet.contains("quantity")) {
-            // There are some parameters
-            String id = request.getParameter("id");
-            int id_1 = Integer.parseInt(id);
-            String name = request.getParameter("name");
-            String description = request.getParameter("description");
-            String quantity = request.getParameter("quantity");
-            int quantity_1 = Integer.parseInt(quantity);
-
-            doUpdate("INSERT INTO sample (id, name, description, quantity) VALUES (?, ?, ?, ?)", id_1, name, description, quantity_1);
-        } else {
-            // There are no parameters at all
-            response.setStatus(500);
-        }
-        return null;
+    public void doPost(String prod){
+        JSONObject jObject = new JSONObject();
+        String name = (String) jObject.get("name");
+        int description = (int) jObject.get("description");
+        int quantity =(int) jObject.get("quantity");
+        doPostPut("INSERT INTO products (name, description, quantity) VALUES (?, ?, ?, ?)", name, description, quantity);
     }
+    
+    private int doPostPut(String query, String name, int description, int quantity){
+        int numChanges = 0;
+        ArrayList prod = new ArrayList();
+        prod.add(name);
+        prod.add(description);
+        prod.add(quantity);
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= prod.size(); i++) {
+                pstmt.setString(i, prod.get(i - 1).toString());
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numChanges;
+    
+    }
+         
+            
+
+    
 
     /**
      *
@@ -93,7 +121,7 @@ public class ProductServlet {
      * @param params
      * @return
      */
-    private int doUpdate(String query, int id, String name, String description, int quantity) {
+    private int doUpdate(String query, int id, String name, int description, int quantity) {
         int numChanges = 0;
         ArrayList prod = new ArrayList();
         prod.add(id);
